209df71984685377e83b41a203cb75b1d5b40e58
Vladimir Starov <starov1987@gmail.com>
2019-10-24 02:20:43 +0700

    create slider works vur.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 import "./scripts/skills";
+import "./scripts/works";
 import "./scripts/parallax";
 
 import Vue from 'vue';
---
+++ src/scripts/works.js
@@ -0,0 +1,86 @@
+import Vue from "vue";
+
+const thumbs = {
+    template: "#slider-thumbs",
+    props: ["works", "currentWork"],
+    computed: {
+        reversedWorks() {
+            return [...this.works].reverse();
+        }
+    }
+}
+
+const btns = {
+    template: "#slider-btns"
+}
+
+const display = {
+    template: "#slider-display",
+    components: {thumbs, btns},
+    props: ["works", "currentWork", "currentIndex"]
+}
+
+const tags = {
+    template: "#slider-tags",
+    props: ["tags"]
+}
+
+const info = {
+    template: "#slider-info",
+    components: { tags },
+    props: ["currentWork"],
+    computed: {
+        tagsArray() {
+            return this.currentWork.skills.split(', ');
+        }
+    }
+}
+
+new Vue({
+    el: "#slider-component",
+    template: "#slider-container",
+    components: {display, info},
+    data: () => ({
+        works: [],
+        currentIndex: 0
+    }),
+    computed: {
+        currentWork() {
+            return this.works[this.currentIndex];
+        }
+    },
+    watch: {
+        currentIndex(value) {
+            this.makeInfiniteLoop(value);
+        }
+    },
+    methods: {
+        makeInfiniteLoop(value) {
+            const worksAmount = this.works.length -1;
+            if (value < 0) this.currentIndex = worksAmount;
+            if (value > worksAmount) this.currentIndex = 0;
+        },
+        makeArrWithRequiredImages(data) {
+            return data.map(item => {
+                const requiredPic = require(`../images/slider/${item.photo}`);
+                item.photo = requiredPic;
+                return item
+            });
+        },
+        handleSlide(direction) {
+            switch(direction) {
+                case "up":
+                    this.currentIndex++;
+                    break;
+                case "down":
+                    this.currentIndex--;
+                    break;
+            }
+            console.log(direction);
+        }
+    },
+    created() {
+        const data = require('../data/works.json');
+        this.works = this.makeArrWithRequiredImages(data);
+    }
+});
